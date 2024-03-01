c031b6952458c9ce4d95c389bd51d17a56422594
ben0222 <20222131100@m.scnu.edu.cn>
2024-03-01 22:42:48 +0800

    v1.0

---
+++ src/.vuepress/config.ts
@@ -0,0 +1,25 @@
+import { defineUserConfig } from "vuepress";
+import theme from "./theme.js";
+
+export default defineUserConfig({
+  // 如果你想让你的网站部署到一个子路径下，你将需要设置它。它的值应当总是以斜杠开始，并以斜杠结束。
+  base: "/",
+
+  locales: {
+    "/": {
+      lang: "en-US",
+      title: "Blog Demo",
+      description: "A blog demo for vuepress-theme-hope",
+    },
+    "/zh/": {
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
+  },
+
+  theme,
+
+  // Enable it with pwa
+  // shouldPrefetch: false,
+});
