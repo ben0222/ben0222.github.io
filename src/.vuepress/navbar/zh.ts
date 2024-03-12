1b2a568fcc19a14f85afa4cb303443636c937d46
ben0222 <20222131100@m.scnu.edu.cn>
2024-03-12 09:54:53 +0800

    Navbar v1.0.1

--- src/.vuepress/navbar/zh.ts
+++ src/.vuepress/navbar/zh.ts
@@ -44,9 +44,69 @@ export const zhNavbar = navbar([
       "strawberry",
     ],
   },
+  // {
+  //   text: "V2 文档",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/zh/",
+  // },
   {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
+    text: "资讯",
+    link: "/zh/intro.html"
+  },
+  {
+    text: "智述",
+    children:
+      [
+
+        // {
+        //   text: "数字非遗博物馆",
+        //   link: "/zh/intro.html"
+        // },
+
+        // {
+        //   text: "古村落语音讲解",
+        //   link: "/zh/intro.html",
+        //   children:
+        //     [
+        //       {
+        //         text: "黎槎古村八卦景区",
+        //         link: "/zh/intro.html"
+        //       },
+        //       {
+        //         text: "赤水塘村",
+        //         link: "/zh/intro.html"
+        //       },
+        //       {
+        //         text: "澄湖村",
+        //         link: "/zh/intro.html"
+        //       },
+        //     ]
+        // }
+        {
+          text: "黎槎古村八卦景区",
+          link: "/zh/intro.html"
+        },
+        {
+          text: "赤水塘村",
+          link: "/zh/intro.html"
+        },
+        {
+          text: "澄湖村",
+          link: "/zh/intro.html"
+        },
+      ]
+
+  },
+  {
+    text: "数字非遗博物馆",
+    link: "/zh/intro.html"
+  },
+  {
+    text: "参观",
+    link: "/zh/intro.html"
+  },
+  {
+    text: "关于",
+    link: "/zh/intro.html",
   },
 ]);
