2ac3ddfd6f2ba2f27fe042d27cf82a19ea3819b5
ben0222 <20222131100@m.scnu.edu.cn>
2024-04-03 20:41:46 +0800

    🦄 refactor: 更改默认语言为中文

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -7,15 +7,17 @@ export default defineUserConfig({
   // 如果你想让你的网站部署到一个子路径下，你将需要设置它。它的值应当总是以斜杠开始，并以斜杠结束。
   base: "/",
   locales: {
+
+    "/zh/": {
+
+      lang: "zh-CN",
+      title: "博客演示",
+      description: "vuepress-theme-hope 的博客演示",
+    },
     "/": {
       lang: "en-US",
       title: "Blog Demo",
       description: "A blog demo for vuepress-theme-hope",
-    },
-    "/zh/": {
-      lang: "zh-CN",
-      title: "博客演示",
-      description: "vuepress-theme-hope 的博客演示",
     },
 
   },
